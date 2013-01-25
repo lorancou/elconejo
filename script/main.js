227ed1b5fd5184472da41313ab492265d550c9ee
Laurent Couvidou <lorancou@free.fr>
2013-01-26 00:09:54 +0100

    Moving character

--- script/main.js
+++ script/main.js
@@ -2,6 +2,7 @@ var stage;
 var wresker;
 
 function init() {
+	
 	// get a reference to the canvas we'll be working with:
 	var canvas = document.getElementById("canvas");
 
@@ -11,16 +12,36 @@ function init() {
 	// create our wresler
 	wresler = new wresler(stage);
 
+	//register key functions
+	document.onkeydown = handleKeyDown;
+	document.onkeyup = handleKeyUp;
+
 	// start game loop
-	createjs.Ticker.setInterval(window.requestAnimationFrame);
+	//createjs.Ticker.setInterval(window.requestAnimationFrame);
+	createjs.Ticker.useRAF = false;
 	createjs.Ticker.addListener(update);
+	createjs.Ticker.setFPS(30);
 }
 
-function update() {
+function update(dt) {
 
 	// game objects updates
-	wresler.update();
+	wresler.update(dt);
 
 	// call update on the stage to make it render the current display list to the canvas:
 	stage.update();
+}
+
+function handleKeyDown(e) {
+
+	//cross browser issues exist
+	if(!e){ var e = window.event; }
+	wresler.handleKeyDown(e);
+}
+
+function handleKeyUp(e) {
+
+	//cross browser issues exist
+	if(!e){ var e = window.event; }
+	wresler.handleKeyUp(e);
 }
--- script/wresler.js
+++ script/wresler.js
@@ -1,7 +1,18 @@
+var KEYCODE_LEFT = 37;
+var KEYCODE_UP = 38;
+var KEYCODE_RIGHT = 39;
+var KEYCODE_DOWN = 40;
+var MOVE_SPEED = 256;
+
 var wresler = function(stage) {
 
     this.x = 320;
     this.y = 240;
+
+    this.lfHeld = false;
+    this.rtHeld = false;
+    this.upHeld = false;
+    this.dwHeld = false;
 
     var ss = new createjs.SpriteSheet({
         "frames": {
@@ -14,16 +25,53 @@ var wresler = function(stage) {
         "images": ["./assets/wresler.png"]
     });
 
-    var bitmapAnimation = new createjs.BitmapAnimation(ss);
-    bitmapAnimation.scaleY = bitmapAnimation.scaleX = 1;
-    bitmapAnimation.gotoAndPlay("run");
+    this.bitmapAnimation = new createjs.BitmapAnimation(ss);
+    this.bitmapAnimation.scaleY = this.bitmapAnimation.scaleX = 1;
+    this.bitmapAnimation.x = 320;
+    this.bitmapAnimation.y = 240;
+    this.bitmapAnimation.gotoAndPlay("run");
 
-    stage.addChild(bitmapAnimation);
+    stage.addChild(this.bitmapAnimation);
                             
     return this;
 };
 
-wresler.prototype.update = function() {
+wresler.prototype.update = function(dt) {
 
+    if (this.lfHeld) {
+        this.bitmapAnimation.x -= MOVE_SPEED * dt / 1000;
+    }
+    if (this.rtHeld) {
+        this.bitmapAnimation.x += MOVE_SPEED * dt / 1000;
+    }
+    if (this.upHeld) {
+        this.bitmapAnimation.y -= MOVE_SPEED * dt / 1000;
+    }
+    if (this.dwHeld) {
+        this.bitmapAnimation.y += MOVE_SPEED * dt / 1000;
+    }
+}
 
+wresler.prototype.handleKeyDown = function(e) {
+
+    switch(e.keyCode) {
+        case KEYCODE_LEFT:  this.lfHeld = true; return false;
+        case KEYCODE_RIGHT: this.rtHeld = true; return false;
+        case KEYCODE_UP:    this.upHeld = true; return false;
+        case KEYCODE_DOWN:  this.dwHeld = true; return false;
+    }
+
+    return true;
+}
+
+wresler.prototype.handleKeyUp = function(e) {
+
+    switch(e.keyCode) {
+        case KEYCODE_LEFT:  this.lfHeld = false; return false;
+        case KEYCODE_RIGHT: this.rtHeld = false; return false;
+        case KEYCODE_UP:    this.upHeld = false; return false;
+        case KEYCODE_DOWN:  this.dwHeld = false; return false;
+    }
+
+    return true;
 }
