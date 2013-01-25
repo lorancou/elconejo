c0700905cb8a86cbf020cb8957d69ac11a6b3e03
Laurent Couvidou <lorancou@free.fr>
2013-01-25 23:18:59 +0100

    Sprite anim

--- script/main.js
+++ script/main.js
@@ -3,30 +3,22 @@ var wresker;
 
 function init() {
 	// get a reference to the canvas we'll be working with:
-	var canvas = document.getElementById("testCanvas");
+	var canvas = document.getElementById("canvas");
 
 	// create a stage object to work with the canvas. This is the top level node in the display list:
 	stage = new createjs.Stage(canvas);
 
-	// Create a new Text object:
-	var text = new createjs.Text("Hello World!", "36px Arial", "#000");
+	// create our wresler
+	wresler = new wresler(stage);
 
-	// add the text as a child of the stage. This means it will be drawn any time the stage is updated
-	// and that it's transformations will be relative to the stage coordinates:
-	stage.addChild(text);
-
-	// position the text on screen, relative to the stage coordinates:
-	text.x = 0;
-	text.y = 0;
-
+	// start game loop
 	createjs.Ticker.setInterval(window.requestAnimationFrame);
 	createjs.Ticker.addListener(update);
-
-	wresler = new wresler();
 }
 
 function update() {
 
+	// game objects updates
 	wresler.update();
 
 	// call update on the stage to make it render the current display list to the canvas:
--- script/wresler.js
+++ script/wresler.js
@@ -1,11 +1,29 @@
-var wresler = function() {
+var wresler = function(stage) {
 
     this.x = 320;
     this.y = 240;
+
+    var ss = new createjs.SpriteSheet({
+        "frames": {
+            "width": 32,
+            "height": 32
+        },
+        "animations": {
+            "run": [0, 1]
+        },
+        "images": ["./assets/wresler.png"]
+    });
+
+    var bitmapAnimation = new createjs.BitmapAnimation(ss);
+    bitmapAnimation.scaleY = bitmapAnimation.scaleX = 1;
+    bitmapAnimation.gotoAndPlay("run");
+
+    stage.addChild(bitmapAnimation);
                             
     return this;
 };
 
 wresler.prototype.update = function() {
-    console.log("coucou");
+
+
 }
